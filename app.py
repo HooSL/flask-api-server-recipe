1c191709466c87967935538733c428590cf66954
HooSL <leegntmd@naver.com>
2022-01-04 11:18:02 +0900

    add

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_restful import Api
 from resources.recipe import RecipeListResource
 from resources.recipe_info import RecipeResource
 from resources.recipe_publish import RecipePublishResouce
+from resources.register import UserRegisterResource
 
 app = Flask(__name__)
 
@@ -16,6 +17,8 @@ api = Api(app)
 api.add_resource(RecipeListResource, '/recipes')
 api.add_resource(RecipeResource,'/recipes/<int:recipe_id>')
 api.add_resource(RecipePublishResouce,'/recipes/<int:recipe_id>/publish')
+api.add_resource(UserRegisterResource,'/user/register')
+
 
 if __name__ == "__main__" :
     app.run()
