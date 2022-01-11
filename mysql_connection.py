c5e21f3dfb23d7eb27f400deb26f7cfe395c92c4
HooSL <leegntmd@naver.com>
2022-01-11 12:14:05 +0900

    add

--- mysql_connection.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    connection=mysql.connector.connect(host = 'dbfoot.cnsfwt1k1yag.ap-northeast-2.rds.amazonaws.com',database='recipe_db',user='recipe_user',password='2105')
-    return connection
